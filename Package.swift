cde64b393a5fe3ab29d1ffb54b1510ebeffa885a
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-06-15 14:27:34 -0400

    Add Swift package

---
+++ Package.swift
@@ -0,0 +1,336 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import CompilerPluginSupport
+import PackageDescription
+
+import class Foundation.FileManager
+import class Foundation.ProcessInfo
+
+// Note: the JAVA_HOME environment variable must be set to point to where
+// Java is installed, e.g.,
+//   Library/Java/JavaVirtualMachines/openjdk-21.jdk/Contents/Home.
+func findJavaHome() -> String {
+  if let home = ProcessInfo.processInfo.environment["JAVA_HOME"] {
+    return home
+  }
+
+  // This is a workaround for envs (some IDEs) which have trouble with
+  // picking up env variables during the build process
+  let path = "\(FileManager.default.homeDirectoryForCurrentUser.path()).java_home"
+  if let home = try? String(contentsOfFile: path, encoding: .utf8) {
+    if let lastChar = home.last, lastChar.isNewline {
+      return String(home.dropLast())
+    }
+
+    return home
+  }
+
+  fatalError("Please set the JAVA_HOME environment variable to point to where Java is installed.")
+}
+let javaHome = findJavaHome()
+
+let javaIncludePath = "\(javaHome)/include"
+#if os(Linux)
+  let javaPlatformIncludePath = "\(javaIncludePath)/linux"
+#elseif os(macOS)
+  let javaPlatformIncludePath = "\(javaIncludePath)/darwin"
+#elseif os(Windows)
+  let javaPlatformIncludePath = "\(javaIncludePath)/win32"
+#endif
+
+let package = Package(
+    name: "JavaLang",
+    platforms: [
+        .macOS(.v10_15)
+    ],
+    products: [
+        .library(
+            name: "JavaLang",
+            targets: ["JavaLang"]
+        ),
+        .library(
+            name: "JavaAWT",
+            targets: ["JavaAWT"]
+        ),
+        .library(
+            name: "JavaBeans",
+            targets: ["JavaBeans"]
+        ),
+        .library(
+            name: "JavaIO",
+            targets: ["JavaIO"]
+        ),
+        .library(
+            name: "JavaMath",
+            targets: ["JavaMath"]
+        ),
+        .library(
+            name: "JavaMath",
+            targets: ["JavaMath"]
+        ),
+        .library(
+            name: "JavaNet",
+            targets: ["JavaNet"]
+        ),/*
+        .library(
+            name: "JavaNIO",
+            targets: ["JavaNIO"]
+        ),
+        .library(
+            name: "JavaSecurity",
+            targets: ["JavaSecurity"]
+        ),*/
+        .library(
+            name: "JavaTime",
+            targets: ["JavaTime"]
+        ),
+        .library(
+            name: "JavaUtil",
+            targets: ["JavaUtil"]
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/PureSwift/swift-java.git",
+            branch: "feature/android-shim"
+        )
+    ],
+    targets: [
+        .target(
+            name: "JavaIO",
+            dependencies: [
+                .product(
+                    name: "JavaKit",
+                    package: "swift-java"
+                ),
+                .product(
+                    name: "JavaKitCollection",
+                    package: "swift-java"
+                ),
+                .product(
+                    name: "JavaKitIO",
+                    package: "swift-java"
+                ),
+            ],
+            exclude: ["swift-java.config"],
+            swiftSettings: [
+              .swiftLanguageMode(.v5),
+            ]
+        ),/*
+        .target(
+            name: "JavaNIO",
+            dependencies: [
+                .product(
+                    name: "JavaKit",
+                    package: "swift-java"
+                ),
+                .product(
+                    name: "JavaKitCollection",
+                    package: "swift-java"
+                ),
+                .product(
+                    name: "JavaKitIO",
+                    package: "swift-java"
+                ),
+                "JavaLang",
+                "JavaIO"
+            ],
+            exclude: ["swift-java.config"],
+            swiftSettings: [
+              .swiftLanguageMode(.v5),
+            ]
+        ),*/
+        .target(
+            name: "JavaLang",
+            dependencies: [
+                .product(
+                    name: "JavaKit",
+                    package: "swift-java"
+                ),
+                .product(
+                    name: "JavaKitCollection",
+                    package: "swift-java"
+                ),
+                .product(
+                    name: "JavaKitReflection",
+                    package: "swift-java"
+                ),
+                .product(
+                    name: "JavaKitIO",
+                    package: "swift-java"
+                ),
+                .product(
+                    name: "JavaKitFunction",
+                    package: "swift-java"
+                ),
+                "JavaIO"
+            ],
+            exclude: ["swift-java.config"],
+            swiftSettings: [
+              .swiftLanguageMode(.v5),
+            ]
+        ),
+        .target(
+            name: "JavaAWT",
+            dependencies: [
+                .product(
+                    name: "JavaKit",
+                    package: "swift-java"
+                ),
+                .product(
+                    name: "JavaKitCollection",
+                    package: "swift-java"
+                ),
+                .product(
+                    name: "JavaKitReflection",
+                    package: "swift-java"
+                ),
+                .product(
+                    name: "JavaKitIO",
+                    package: "swift-java"
+                ),
+                "JavaIO",
+                "JavaLang"
+            ],
+            exclude: ["swift-java.config"],
+            swiftSettings: [
+              .swiftLanguageMode(.v5),
+            ]
+        ),
+        .target(
+            name: "JavaTime",
+            dependencies: [
+                .product(
+                    name: "JavaKit",
+                    package: "swift-java"
+                ),
+                .product(
+                    name: "JavaKitCollection",
+                    package: "swift-java"
+                ),
+                .product(
+                    name: "JavaKitReflection",
+                    package: "swift-java"
+                ),
+                .product(
+                    name: "JavaKitIO",
+                    package: "swift-java"
+                ),
+                "JavaLang",
+                "JavaIO",
+            ],
+            exclude: ["swift-java.config"],
+            swiftSettings: [
+              .swiftLanguageMode(.v5),
+            ]
+        ),
+        .target(
+            name: "JavaNet",
+            dependencies: [
+                .product(
+                    name: "JavaKit",
+                    package: "swift-java"
+                ),
+                .product(
+                    name: "JavaKitCollection",
+                    package: "swift-java"
+                ),
+                .product(
+                    name: "JavaKitReflection",
+                    package: "swift-java"
+                ),
+                .product(
+                    name: "JavaKitIO",
+                    package: "swift-java"
+                ),
+                .product(
+                    name: "JavaKitNetwork",
+                    package: "swift-java"
+                ),
+                "JavaIO",
+                "JavaUtil"
+            ],
+            exclude: ["swift-java.config"],
+            swiftSettings: [
+              .swiftLanguageMode(.v5),
+            ]
+        ),
+        .target(
+            name: "JavaUtil",
+            dependencies: [
+                .product(
+                    name: "JavaKit",
+                    package: "swift-java"
+                ),
+                .product(
+                    name: "JavaKitCollection",
+                    package: "swift-java"
+                ),
+                .product(
+                    name: "JavaKitReflection",
+                    package: "swift-java"
+                ),
+                .product(
+                    name: "JavaKitFunction",
+                    package: "swift-java"
+                ),
+                .product(
+                    name: "JavaKitIO",
+                    package: "swift-java"
+                ),
+                "JavaIO",
+                "JavaLang",
+                "JavaTime"
+            ],
+            exclude: ["swift-java.config"],
+            swiftSettings: [
+              .swiftLanguageMode(.v5),
+            ]
+        ),
+        .target(
+            name: "JavaBeans",
+            dependencies: [
+                .product(
+                    name: "JavaKit",
+                    package: "swift-java"
+                ),
+                .product(
+                    name: "JavaKitCollection",
+                    package: "swift-java"
+                ),
+                .product(
+                    name: "JavaKitReflection",
+                    package: "swift-java"
+                ),
+                .product(
+                    name: "JavaKitFunction",
+                    package: "swift-java"
+                ),
+                .product(
+                    name: "JavaKitIO",
+                    package: "swift-java"
+                ),
+                "JavaIO"
+            ],
+            exclude: ["swift-java.config"],
+            swiftSettings: [
+              .swiftLanguageMode(.v5),
+            ]
+        ),
+        .target(
+            name: "JavaMath",
+            dependencies: [
+                "JavaLang"
+            ],
+            exclude: ["swift-java.config"],
+            swiftSettings: [
+              .swiftLanguageMode(.v5),
+            ]
+        ),
+        .testTarget(
+            name: "JavaLangTests",
+            dependencies: ["JavaLang"]
+        )
+    ]
+)
