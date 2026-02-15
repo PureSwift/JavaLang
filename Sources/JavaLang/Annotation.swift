f1c06f2b75a555ab942b31622f29c6a5ea67cfc1
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-02-15 12:37:42 -0500

    Remove duplicate file

--- Sources/JavaLang/Annotation.swift
+++
@@ -1,7 +0,0 @@
-import SwiftJava
-
-@JavaInterface("java.lang.annotation.Annotation")
-public struct Annotation: AnyJavaObject {
-  @JavaMethod
-  public func annotationType() -> JavaClass<Annotation>!
-}
