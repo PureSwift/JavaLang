baad5eeb01d974d011b74d45b1a0df37b858ac06
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-02-15 11:43:09 -0500

    Remove duplicate file

--- Sources/JavaLang/FileDescriptor.swift
+++
@@ -1,7 +0,0 @@
-import SwiftJava
-
-@JavaClass("java.io.FileDescriptor")
-open class FileDescriptor: JavaObject {
-  @JavaMethod
-  @_nonoverride public convenience init(environment: JNIEnvironment? = nil)
-}
